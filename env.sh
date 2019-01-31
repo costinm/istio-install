511a3de85eea3a4a499d20f8cb6df7e6c0a8303c
Costin Manolache <costin@gmail.com>
2019-01-30 17:06:07 -0800

    More fixes and refactoring

--- env.sh
+++ env.sh
@@ -42,16 +42,33 @@ fi
 ISTIO_CONFIG=${ISTIO_CONFIG:-${IBASE}/user-values.yaml}
 
 
-HUB=${HUB:-grc.io/istio-release}
+# The file contains examples of various setup commands I use on test clusters.
+#
+# The examples in this file will create a number of control plane profiles:
+#
+# istio-control - the main control plane, 1.1 based
+# istio-pilot11 - additional example control plane, 1.1 based, can be used for canary/testing without changing control
+#    (TODO: rename to istio-control-staging ?)
+# istio-master - based on master
+# istio-env11  - example with just sidecar injector ( for remote clusters not running full pilot for example )
+#
+# In addition, the istio-ingress and istio-ingress-insecure run their own dedicated pilot, which is needed for
+# k8s ingress support and is an example on how to segregate sidecar and gateway pilot (same config - but different replicas)
+#
+# It creates a number of gateways:
+# istio-ingress - k8s ingress + gateway, mtls enabled
+# istio-ingress-insecure - k8s ingress + gateway, mtls and certificates off, can only connect to permissive (example for old no-mtls -)
+#    Notice the sidecar
+# istio-ingress-master - ingress using master control plane
+#
+# It has 2 telemetry servers:
+# istio-telemetry - used for all control planes except istio-master
+# istio-telemetry-master - example of separate telemetry server, using istio-master env and accessed by istio-ingress-master.
 
-
-# TAGs will default to 'release1.1-latest-daily'
-
-# Typical installation
+# Typical installation, similar with istio normal install but using different namespaces/components.
 function iop_all() {
     iop_system_citadel $*
     iop_control $*
-    iop_ingress_pilot $*
     iop_ingress $*
     iop_telemetry $*
     iop_policy $*
@@ -65,6 +82,26 @@ function iop_tst_env() {
     # Under istio-ingress-insecure, mtls off, not using certs.
     iop_ingress_pilot_insecure $*
     iop_ingress_insecure $*
+}
+
+# Install an istio_master env, control plane plus telemetry.
+# Uses system citadel.
+# Policy is disabled in this environment
+function iop_master() {
+    TAG=master-latest-daily HUB=gcr.io/istio-release istioop istio-master $IBASE/istio-control \
+       --set policy.enable=false \
+       --set global.istioNamespace=istio-master \
+       --set global.telemetryNamespace=istio-telemetry-master
+
+    TAG=master-latest-daily HUB=gcr.io/istio-release istioop istio-telemetry-master $IBASE/istio-telemetry \
+        --set global.istioNamespace=istio-master \
+        $*
+
+    # Gateway for master - no ingress support (will be dropped)
+    TAG=master-latest-daily HUB=gcr.io/istio-release istioop istio-gateway-master $IBASE/istio-ingress \
+        --set global.istioNamespace=istio-master \
+        $*
+
 }
 
 # Can run side-by-side with istio 1.0 or 1.1 - just runs citadel.
@@ -90,19 +127,19 @@ function iop_control() {
 # Ingress 1.1 in istio-ingress namespace, with k8s ingress and dedicated pilot
 # istio-system may have a second ingress.
 function iop_ingress() {
-    # TODO: customize test domains for each ingress namespace (move to separate function )
-
     # Normal 1.1 ingress. Defaults to istio-control control plane.
+    istioop istio-ingress $IBASE/istio-ingress \
+        --set global.istioNamespace=istio-control \
+        $*
+}
+
+# Example for an Ingress that supports legacy K8S Ingress. Uses a dedicated pilot.
+function iop_k8s_ingress() {
     istioop istio-ingress $IBASE/istio-ingress \
         --set k8sIngress=true \
         --set global.istioNamespace=istio-ingress \
         $*
-}
 
-function iop_ingress_pilot() {
-    # TODO: customize test domains for each ingress namespace (move to separate function )
-
-    # Same namespace pilot, with ingress controller activated
     # No MCP or injector - dedicated for the gateway ( perf and scale characteristics are different from main pilot,
     # and we may want custom settings anyways )
     NAMESPACE=istio-ingress istioop istio-ingress-pilot $IBASE/istio-control \
@@ -112,8 +149,8 @@ function iop_ingress_pilot() {
          --set sidecarInjectorWebhook.enabled=false \
          --set galley.enabled=false \
           $*
-
 }
+
 
 # Verify we can install a second ingress.
 # This must be in STRTICT mode to avoid conflicts
@@ -221,7 +258,7 @@ function iop_cni() {
 
 function iop_load() {
     kubectl create ns load
-    kubectl label namespace load istio-injection=enabled
+    kubectl label namespace load istio-env=istio-control
 
     istioop load $IBASE/test/pilotload $*
 }
@@ -263,12 +300,12 @@ function logs-inject11() {
     _klog istio=sidecar-injector sidecar-injector-webhook istio-pilot11 $*
 }
 
-function logs-pilot11() {
-    _klog istio=pilot discovery istio-pilot11 $*
+function logs-pilot() {
+    _klog istio=pilot discovery ${NS:-istio-control} $*
 }
 
 function exec-pilot11() {
-    _kexec istio=pilot discovery istio-pilot11 $*
+    _kexec istio=pilot discovery ${NS:-istio-control}  $*
 }
 
 function logs-fortio11() {
@@ -297,7 +334,10 @@ function iop_test_apps() {
     #kubectl -n test apply -f samples/httpbin/httpbin.yaml
     #kubectl -n bookinfo apply -f samples/bookinfo/kube/bookinfo.yaml
 
-    helm install -n fortio10 --namespace fortio10 test/fortio
+    kubectl create ns fortio-control
+    kubectl label ns fortio-control istio-env=istio-control
+
+    helm install -n fortio-control --namespace fortio-control test/fortio
 
 }
 
@@ -343,6 +383,7 @@ function getCertLegoInit() {
 function getCertLego() {
  # GCP_PROJECT=costin-istio
  # DOMAIN=istio.webinf.info
+ # NAMESPACE - where to create the secret
 
  #gcloud dns record-sets list --zone ${DNS_ZONE}
 
@@ -352,7 +393,7 @@ function getCertLego() {
  --dns="gcloud"     \
  --path="${HOME}/.lego"  run
 
- kubectl create -n istio-ingress secret tls istio-ingressgateway-certs --key ${HOME}/.lego/certificates/_.${DOMAIN}.key \
+ kubectl create -n ${NAMESPACE:-istio-ingress} secret tls istio-ingressgateway-certs --key ${HOME}/.lego/certificates/_.${DOMAIN}.key \
     --cert ${HOME}/.lego/certificates/_.${DOMAIN}.crt
 
 }
@@ -363,13 +404,16 @@ function getCertLego() {
 # You need to either buy a domain from google or set the DNS to point to gcp.
 # Similar scripts can setup DNS using a different provider
 function testCreateDNS() {
+    # TODO: cleanup, pretty convoluted
+    # GCP_PROJECT=costin-istio DOMAIN=istio.webinf.info IP=35.222.25.73 testCreateDNS control
+    # will create ingresscontrol and *.control CNAME.
     local ver=${1:-v10}
     local name=ingress${ver}
 
     gcloud dns --project=$GCP_PROJECT record-sets transaction start --zone=$DNS_ZONE
 
     gcloud dns --project=$GCP_PROJECT record-sets transaction add $IP --name=${name}.${DOMAIN}. --ttl=300 --type=A --zone=$DNS_ZONE
-    gcloud dns --project=$GCP_PROJECT record-sets transaction add ${name}.${DOMAIN} --name="*.${DOMAIN}." \
+    gcloud dns --project=$GCP_PROJECT record-sets transaction add ${name}.${DOMAIN}. --name="*.${ver}.${DOMAIN}." \
         --ttl=300  --type=CNAME --zone=$DNS_ZONE
 
     gcloud dns --project=$GCP_PROJECT record-sets transaction execute --zone=$DNS_ZONE
