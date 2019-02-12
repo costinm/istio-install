4b3d63ad73c8d9e863017533587617b51bd510cd
Costin Manolache <costin@gmail.com>
2019-02-12 10:55:39 -0800

    Update/fix more tests, adding Sidecar and support for 'prune'

--- test/k8s/minimal-ingress/setup.sh
+++ test/k8s/minimal-ingress/setup.sh
@@ -4,7 +4,9 @@ kubectl create ns istio-micro-ingress 2>/dev/null
 kubectl create ns test-micro-ingress 2>/dev/null
 
 # --prune --all --cascade=true  -> doesn't work across namespaces
-kubectl apply --grace-period=4 -n istio-micro-ingress -f test/k8s/minimal-ingress/istio-micro-ingress
-kubectl apply --grace-period=4 -n test-micro-ingress -f test/k8s/minimal-ingress/test-micro-ingress
+cat test/k8s/minimal-ingress/istio-micro-ingress/* \
+  | sed s/example.com/${DOMAIN:-example.com}/ | kubectl apply --grace-period=4 -n istio-micro-ingress -f -
+cat test/k8s/minimal-ingress/test-micro-ingress/* \
+  | sed s/example.com/${DOMAIN:-example.com}/ | kubectl apply --grace-period=4 -n test-micro-ingress -f -
 
 kubectl get nodes -o wide
